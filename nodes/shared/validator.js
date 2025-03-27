d8b3d68bbf83d9b97a7d513d96e985e597d0c323
David Kartnaller <david.kartnaller@gmail.com>
2025-03-27 11:33:43 +0000

    improve error reporting

--- nodes/shared/validator.js
+++ nodes/shared/validator.js
@@ -4,9 +4,17 @@
  * @property {string} key 
  * @property {string|string[]} type 
  * @property {any} [fallback]
- * @property {(value: any) => true|Error} [validator]
+ * @property {(value: any, location?: string) => true|Error} [validator]
  */
 
+/**
+ * Gets the location description
+ * @param {string} [location]
+ * @returns {string}
+ */
+function getLocation(location) {
+  return location ? ` on .${location}` : ""
+}
 
 
 /**
@@ -20,7 +28,7 @@ function getValue({ payload, key, type, fallback, validator }) {
     throw new Error(`no value given for key ${key}`)
   }
   expectType(type, payload[key], key)
-  if (typeof validator === "function") validator(payload[key])
+  if (typeof validator === "function") validator(payload[key], key)
   return payload[key]
 }
 
@@ -33,61 +41,64 @@ function getValue({ payload, key, type, fallback, validator }) {
 function expectType(types, value, location) {
   if (!Array.isArray(types)) types = [types]
   if (types.includes(typeof value)) return true
-  throw Error(`invalid type: expected ${types.join(" or ")} but got ${typeof value}${location ? ` on .${location}` : ""}`)
+  throw Error(`expected ${types.join(" or ")} but got ${typeof value}${getLocation(location)}`)
 }
 
 /**
  * checks if the specified numer is in range
  * @param {number} min 
  * @param {number} max 
- * @returns {(value: any) => true}
+ * @returns {(value: any, location?: string) => true}
  */
 function inRange(min, max) {
   /**
    * @param {any} value
+   * @param {string} [location]
    * @returns {true}
    */
-  return value => {
+  return (value, location) => {
     expectType("number", value)
     if (value >= min && value <= max) return true
-    if (value < min) throw new Error(`value ${value} is too low`)
-    if (value > max) throw new Error(`value ${value} is too high`)
-    throw new Error("something went wrong")
+    if (value < min) throw new Error(`value ${value} must be >= ${min}${getLocation(location)}`)
+    if (value > max) throw new Error(`value ${value} must be <= ${max}${getLocation(location)}`)
+    throw new Error(`something went wrong${getLocation(location)}`)
   }
 }
 
 /**
  * checks for a string match
  * @param {RegExp} regex
- * @returns {(value: any) => true}
+ * @returns {(value: any, location?: string) => true}
  */
 function match(regex) {
   /**
    * @param {any} value
+   * @param {string} [location]
    * @returns {true}
    */
-  return value => {
+  return (value, location) => {
     expectType("string", value)
     if (regex.test(value)) return true
-    throw new Error(`invalid string: "${value}", should match ${regex}`)
+    throw new Error(`"${value}", should match ${regex}${getLocation(location)}`)
   }
 }
 
 /**
  * 
- * @param  {...(value: any) => true} funcs 
+ * @param  {...(value: any, location: string|undefined) => true} funcs 
  */
 function or(...funcs) {
   /**
    * @param {any} value
+   * @param {string} [location]
    * @returns {true}
    */
-  return value => {
+  return (value, location) => {
     /** @type {Error[]} */
     const errors = []
     const ok = funcs.some(f => {
       try {
-        return f(value)
+        return f(value, location)
       } catch (e) {
         if (e instanceof Error) errors.push(e)
         return false
